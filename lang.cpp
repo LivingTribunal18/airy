ccc3d193c0f00529e716b8bcb6df8e976cab5e01
LivingTribunal18 <46354384+LivingTribunal18@users.noreply.github.com>
2019-01-04 20:23:20 +0500

    Parser
    splits the contents of the file into tokens

---
+++ lang.cpp
@@ -0,0 +1,63 @@
+// модификация 1 ;
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+char operators[] = {':'};
+bool isOperator(char word){
+  for (int i = 0; i < sizeof(operators)/sizeof(char); i++) {
+    if (word == operators[i]){
+      return true;
+    }
+  }
+  return false;
+}
+char separators[] = {';', '!'};
+bool isSeparator(char list){
+  for (int i = 0; i < sizeof(separators)/sizeof(char); i++) {
+    if (list == separators[i]){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+int main(){
+  ifstream code("1.txt");
+  vector<string> code_list;
+  string word = "";
+
+  char ch;
+  while (!code.eof()) {
+      code >> ch;
+      if((isOperator(ch)) || (isSeparator(ch))){
+        code_list.push_back(word);
+        word = ch;
+        code_list.push_back(word);
+        word = "";
+      }else{
+        word += ch;
+      }
+  }
+  if(word != ""){
+    code_list.push_back(word);
+  }
+
+  code_list.pop_back();
+  code_list.pop_back();
+
+  for(int i = 0; i < code_list.size(); i++){
+    cout << code_list[i] << endl;
+  }
+
+
+code.close();
+
+return 0;
+}
