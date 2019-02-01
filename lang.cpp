1f807bb9856418f8b6392d2c7db3f69274f0aab8
LivingTribunal18 <46354384+LivingTribunal18@users.noreply.github.com>
2019-02-01 21:37:38 +0500

    Add files via upload
    Fixed writing to a binary file.

--- lang.cpp
+++ lang.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <algorithm>
 #include <bits/stdc++.h>
+#include <ctype.h>
 
 
 using namespace std;
@@ -70,6 +71,36 @@ void push(string word){
   }
 }
 
+int asci = 0;
+void ascii(string letter){
+  if(letter == "a") {asci = 0x61;}
+  else if(letter == "b") {asci = 0x62;}
+  else if(letter == "c") {asci = 0x63;}
+  else if(letter == "d") {asci = 0x64;}
+  else if(letter == "e") {asci = 0x65;}
+  else if(letter == "f") {asci = 0x66;}
+  else if(letter == "g") {asci = 0x67;}
+  else if(letter == "h") {asci = 0x68;}
+  else if(letter == "i") {asci = 0x69;}
+  else if(letter == "j") {asci = 0x6A;}
+  else if(letter == "k") {asci = 0x6B;}
+  else if(letter == "l") {asci = 0x6C;}
+  else if(letter == "m") {asci = 0x6D;}
+  else if(letter == "n") {asci = 0x6E;}
+  else if(letter == "o") {asci = 0x6F;}
+  else if(letter == "p") {asci = 0x70;}
+  else if(letter == "q") {asci = 0x71;}
+  else if(letter == "r") {asci = 0x72;}
+  else if(letter == "s") {asci = 0x73;}
+  else if(letter == "t") {asci = 0x74;}
+  else if(letter == "u") {asci = 0x75;}
+  else if(letter == "v") {asci = 0x76;}
+  else if(letter == "w") {asci = 0x77;}
+  else if(letter == "x") {asci = 0x78;}
+  else if(letter == "y") {asci = 0x79;}
+  else if(letter == "z") {asci = 0x7A;}
+}
+
 int main(){
   FILE *code;
   if((code = fopen("1.txt", "r")) == NULL) {
@@ -105,6 +136,7 @@ int main(){
 
   ofstream vm_code("2.txt", ios::binary);
 
+
   if (!vm_code.is_open()){
       cout << "Cannot write in file \n";
       return 2;
@@ -122,26 +154,29 @@ int halt = 0x00;
 
 vector<int> counter(5, 0);
 
+string tok = "Literal", tok1 = "Identifier";
+
+
 for(int i = 0; i < code_tokens.size(); i++){
    for(int j = 0; j < vm_comands.size(); j++){
       if (code_tokens[i] == vm_comands[j]) {
          counter[0]++;
       }
       if (counter[0] >= 1) {
-           int lit_it = find(code_tokens[i].begin(), code_tokens[i].end(), "Literal") - code_tokens[i].begin();
-           int id_it = find(code_tokens[i].begin(), code_tokens[i].end(), "Identifier") - code_tokens[i].begin();
+           int lit_it = find(code_tokens[i].begin(), code_tokens[i].end(), tok) - code_tokens[i].begin();
+           int id_it = find(code_tokens[i].begin(), code_tokens[i].end(), tok1) - code_tokens[i].begin();
            vm_code.write((char*)&push_byte, sizeof(push_byte));
-           vm_code.write((char*)&code_list[i][lit_it], sizeof(code_list[i][lit_it]));
+           int stoi = atoi(code_list[i][lit_it].c_str());
+           vm_code.write((char*)&stoi, sizeof(stoi));
+           stoi = 0;
            vm_code.write((char*)&store_byte, sizeof(store_byte));
-           vm_code.write((char*)&code_list[i][id_it], sizeof(code_list[i][id_it]));
+           ascii(code_list[i][id_it]);
+           vm_code.write((char*)&asci, sizeof(asci));
       }
    }
 }
 
 vm_code.write((char*)&halt, sizeof(halt));
-
-
-
 
   vm_code.close();
   fclose (code);
